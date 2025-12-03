6e0bb47c82a7e2934b9b15b6bc3a651b074a0394
mahderhawz <mahderhawaz16@gmail.com>
2025-12-03 22:45:43 +0300

    fix: update user schema to proper NestJS mongoose pattern

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -3,12 +3,12 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcryptjs';
 import * as jwt from 'jsonwebtoken';
-import { User } from '../schemas/user.schema';
+import { User, UserDocument } from '../schemas/user.schema';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async login(loginDto: LoginDto) {
     const { email, password } = loginDto;
