cbe54fa7ec7b175dbfdf755f7578d0efef0032c9
mahderhawz <mahderhawaz16@gmail.com>
2025-12-03 22:13:38 +0300

    refactor: clean login branch - remove investors and scheduler

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,15 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ScheduleModule } from '@nestjs/schedule';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { InvestorsModule } from './investors/investors.module';
-import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ScheduleModule.forRoot(),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -22,8 +18,6 @@ import { SchedulerModule } from './scheduler/scheduler.module';
       }),
     }),
     AuthModule,
-    InvestorsModule,
-    SchedulerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- server/src/investors/dto/update-investor.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInvestorDto } from './create-investor.dto';
-
-export class UpdateInvestorDto extends PartialType(CreateInvestorDto) {}
