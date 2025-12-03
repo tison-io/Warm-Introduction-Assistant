a0549438933a8503aa8b07f2ce93496596118791
mahderhawz <mahderhawaz16@gmail.com>
2025-12-04 00:01:27 +0300

    Resolve all merge conflicts

--- server/src/schemas/investor.schema.ts
+++ server/src/schemas/investor.schema.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
-<<<<<<< HEAD
 @Schema({ timestamps: true })
 export class Investor extends Document {
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
@@ -18,26 +17,6 @@ export class Investor extends Document {
 
   @Prop()
   notes?: string;
-=======
-export type InvestorDocument = Investor & Document;
-
-@Schema({ timestamps: true })
-export class Investor {
-  @Prop({ required: true })
-  name!: string;
-
-  @Prop({ type: [String], default: [] })
-  tags!: string[];
-
-  @Prop({ required: true })
-  preferred_intro_format!: string;
-
-  @Prop()
-  notes?: string;
-
-  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
-  ownerId!: Types.ObjectId;
->>>>>>> feature/investors
 }
 
 export const InvestorSchema = SchemaFactory.createForClass(Investor);
