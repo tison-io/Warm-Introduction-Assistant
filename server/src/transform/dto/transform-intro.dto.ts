d0fc166ab31a8c7eafb1d06e08a54ca2db71e97d
Ashbel King’ori <kashbel747@gmail.com>
2025-12-11 13:03:20 +0300

    Add backend integration to Gen AI transform endpoint

--- server/src/transform/dto/transform-intro.dto.ts
+++ server/src/transform/dto/transform-intro.dto.ts
@@ -2,29 +2,8 @@ import { IsString } from "class-validator";
 
 export class TransformIntroDto {
     @IsString()
-    startup_id: string;
+    blurb: string;
 
     @IsString()
-    startup_name: string;
-
-    @IsString()
-    startup_pitch_link: string;
-
-    @IsString()
-    startup_blurb: string;
-
-    @IsString()
-    investor_id: string;
-
-    @IsString()
-    investor_name: string;
-
-    @IsString()
-    preferred_intro_format: string;
-
-    @IsString()
-    intro_preferences_text: string;
-
-    @IsString()
-    founder_id: string; 
+    investor_preference: string; 
 }
