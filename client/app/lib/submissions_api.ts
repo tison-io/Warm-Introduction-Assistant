815ae26e4e75ce384b62d585e6a622b9149e569f
Ashbel King’ori <kashbel747@gmail.com>
2025-12-17 14:09:54 +0300

    Resolve env config for submissions-api

--- client/app/lib/submissions_api.ts
+++ client/app/lib/submissions_api.ts
@@ -1,6 +1,6 @@
 import { ContactFormData } from "../types/contacts";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+const API_URL = process.env.NEXT_PUBLIC_FOUNDER_API_URL || "http://localhost:4000";
 
 export async function sendContactForm(data: ContactFormData) {
   try {
